428992cdc16600f9b6c270635b932b4b331fcf9d
kuma033 <nhayato28@yahoo.co.jp>
2022-10-01 18:02:17 +0900

    db接続完了

--- server/main.go
+++ server/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+	"github.com/kuma0328/kyopuroQA/server/db"
+)
+
 func main() {
-	
+	// envファイル読み込み
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("envファイルが読み込めませんでした:%v", err)
+	}
+	//db接続
+	db := db.NewDB()
+	defer db.Conn.Close()
+
+	router := router
 }
